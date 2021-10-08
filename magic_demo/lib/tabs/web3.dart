74c677aa8ffb75cc6ae05f5e84e23c0904891c2f
Ethella <jerry@fortmatic.com>
2021-10-07 22:08:36 -0700

    Remove not needed comments

--- magic_demo/lib/tabs/web3.dart
+++ magic_demo/lib/tabs/web3.dart
@@ -13,15 +13,6 @@ import '../alert.dart';
 class Web3Page extends StatefulWidget {
   const Web3Page({Key? key}) : super(key: key);
 
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
   @override
   State<Web3Page> createState() => _Web3PageState();
 }
@@ -33,11 +24,6 @@ class _Web3PageState extends State<Web3Page> {
   final credential = MagicCredential(Magic.instance.provider);
 
   final myController = TextEditingController(text: 'jerry@magic.link');
-
-  // @override
-  // Future<void> initState() {
-  //   super.initState();
-  // }
 
   @override
   Widget build(BuildContext context) {
