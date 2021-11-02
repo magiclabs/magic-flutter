b2d1b7a22578a808d6a2a8068122032744bc64c7
Ethella <jerry@fortmatic.com>
2021-11-02 07:51:08 -0700

    Add comments

--- packages/magic_ext_oauth/lib/types/oauth_method.dart
+++ packages/magic_ext_oauth/lib/types/oauth_method.dart
@@ -1,5 +1,6 @@
 part of '../magic_ext_oauth.dart';
 
+/// OAuth method for internal usage
 enum OAuthMethod { magic_oauth_parse_redirect_result }
 
 extension ParseOAuthMethodToString on OAuthMethod {
