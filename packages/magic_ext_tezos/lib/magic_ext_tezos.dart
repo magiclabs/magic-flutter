75b5d2b55c3c9ab976c78bea73aaa053d5f323c1
ethella <jerry@magic.link>
2022-05-19 15:41:07 -0700

    rename signAsync to sign in tezos

--- packages/magic_ext_tezos/lib/magic_ext_tezos.dart
+++ packages/magic_ext_tezos/lib/magic_ext_tezos.dart
@@ -1,7 +1,8 @@
 import 'dart:convert';
 import 'dart:typed_data';
 
-import 'package:blockchain_signer/signer/external_signer.dart';
+import 'package:blockchain_signer/signer/remote/remote_signer.dart';
+import 'package:blockchain_signer/signer/sign_result.dart';
 import 'package:magic_sdk/magic_sdk.dart';
 import 'package:magic_sdk/modules/base_module.dart';
 import 'package:magic_sdk/provider/rpc_provider.dart';
@@ -10,7 +11,7 @@ import 'package:magic_sdk/provider/types/relayer_response.dart';
 part 'types/tezos_method.dart';
 
 /// Magic Signer re-routes signing requests to auth relayer and trigger ledger bridge signing
-class TezosExtension extends BaseModule implements ExternalSigner  {
+class TezosExtension extends BaseModule implements RemoteSigner  {
 
   TezosExtension(RpcProvider provider) : super(provider);
 
@@ -19,7 +20,7 @@ class TezosExtension extends BaseModule implements ExternalSigner  {
   /// @param [op] Operation to sign
   /// @param [magicByte] Magic bytes 1 for block, 2 for endorsement, 3 for generic, 5 for the PACK format of michelson
   @override
-  Future<String> signAsync(String op, Uint8List magicByte){
+  Future<SignResult> sign(String op, Uint8List magicByte){
     final params = {
       "bytes": op,
       "watermark": magicByte
@@ -28,11 +29,23 @@ class TezosExtension extends BaseModule implements ExternalSigner  {
     return sendToProvider(
         method: TezosMethod.taquito_sign,
         params: [params]).then((jsMsg) {
-      var relayerResponse = RelayerResponse<String>.fromJson(
-          json.decode(jsMsg.message), (json) => json as String);
+      var relayerResponse = RelayerResponse<SignResult>.fromJson(
+          json.decode(jsMsg.message),
+              (json) => SignResult.fromJson(json as Map<String, dynamic>));
       return relayerResponse.response.result;
     });
   }
+
+  @override
+  // TODO: implement address
+  Future<String> get address => throw UnimplementedError();
+
+  @override
+  // TODO: implement publicKey
+  Future<String> get publicKey => throw UnimplementedError();
+
+  @override
+  Future<String> get secretKey => throw UnsupportedError('Secret key is not available');
 }
 
 /// Append basic modules on runtime
