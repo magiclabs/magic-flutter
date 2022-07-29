44234f24df6756e95383beab349d4a3340544835
ethella <jerry@magic.link>
2022-07-29 11:53:55 -0700

    Implement nested toJSON

--- packages/magic_sdk/lib/provider/rpc_provider.dart
+++ packages/magic_sdk/lib/provider/rpc_provider.dart
@@ -19,14 +19,15 @@ class RpcProvider implements RpcService {
   RpcProvider(this._overlay);
 
   /// Sends message to relayer
-  Future<JavascriptMessage> send(
+  Future<JavascriptMessage> send (
       {required MagicRPCRequest request,
-      required Completer<JavascriptMessage> completer}) {
+      required Completer<JavascriptMessage> completer}) async {
     var msgType = OutboundMessageType.MAGIC_HANDLE_REQUEST;
+    var encodedParams = await URLBuilder.instance.encodedParams;
 
     var relayerRequest = RelayerRequest(
         msgType:
-            '${msgType.toString().split('.').last}-${URLBuilder.instance.encodedParams}',
+            '${msgType.toString().split('.').last}-$encodedParams',
         payload: request);
 
     _overlay.enqueue(
